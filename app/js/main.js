1998c54074c337d776305b8df68be0454241c78e
Pivodur <megazyamba@gmail.com>
2023-03-01 20:07:41 +0600

    fix: add mixitup for design

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,25 @@ $(function(){
       autoplay: true,
       autoplaySpeed: 2000,
    })
-   var mixer = mixitup('.products-top__items');
+
+   var mixer1 = mixitup('.products', {
+      controls: {
+         scope: 'local'
+      }
+   })
+   var mixer2 = mixitup('.design', {
+      controls: {
+         scope: 'local'
+      }
+   })
+   
+   // let containerEl1 = document.querySelector('.design');
+   // let containerEl2 = document.querySelector('.products');
+   // let config = {
+   //     controls: {
+   //         scope: 'local',
+   //     },
+   // };
+   // (() => mixitup(containerEl1, config))();
+   // (() => mixitup(containerEl2, config))();
 });
